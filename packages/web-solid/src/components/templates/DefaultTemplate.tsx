9a2a84f1f30f9ab372ebcc7bd86398fdf3dc9c41
kurone-kito <krone@kit.black>
2025-07-07 01:55:59 +0900

    feat(root,web): implemented the metadata of the website

--- packages/web-solid/src/components/templates/DefaultTemplate.tsx
+++ packages/web-solid/src/components/templates/DefaultTemplate.tsx
@@ -1,6 +1,8 @@
+import { MetaProvider } from '@solidjs/meta';
 import type { RouteSectionProps } from '@solidjs/router';
 import type { Component } from 'solid-js';
 import { Suspense } from 'solid-js';
+import { Head } from '../organisms/Head.js';
 
 /**
  * The default template component.
@@ -8,7 +10,10 @@ import { Suspense } from 'solid-js';
  * @returns The component.
  */
 export const DefaultTemplate: Component<RouteSectionProps> = (props) => (
-  <Suspense>
-    <main>{props.children}</main>
-  </Suspense>
+  <MetaProvider>
+    <Head />
+    <Suspense>
+      <main>{props.children}</main>
+    </Suspense>
+  </MetaProvider>
 );
